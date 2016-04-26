8ede2cd23a197be587e760fd94d69e1721751112
Danny <danny@skully.com>
2016-04-26 12:19:02 -0700

    Added in banner on HUD

---
+++ Assets/Scripts/TextUpdate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextUpdate : MonoBehaviour {
+	
+	Text mph;
+
+	// Use this for initialization
+	void Start () {
+		mph = GetComponent<Text>();
+		StartCoroutine(WaitFor());
+	
+	}
+	
+	IEnumerator WaitFor() {
+		yield return new WaitForSeconds(2);
+		int temp = int.Parse(mph.text);
+		mph.text = (temp + 3).ToString();
+		StopCoroutine(WaitFor());
+		StartCoroutine(WaitFor());
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
