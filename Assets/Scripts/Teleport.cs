80ac1b9092a5832dfa587fc2986c17fa2a5a1c79
Daniel Gillies <danny@skully.com>
2016-04-27 01:37:17 -0700

    almost got timer working

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -15,94 +15,119 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Collider))]
-public class Teleport : MonoBehaviour, ICardboardGazeResponder {
-  private Vector3 startingPosition;
-  private Vector3 smallThumb;
-  private Vector3 largeThumb;
+[RequireComponent (typeof(Collider))]
+public class Teleport : MonoBehaviour, ICardboardGazeResponder
+{
+	private Vector3 startingPosition;
+	private Vector3 smallThumb;
+	private Vector3 largeThumb;
+	public CardboardHead head;
+	private float delay = 0.0f;
+	public int sceneNum;
+	private LoadOnClick sceneLoader;
 
-  void Start() {
-    startingPosition = transform.localPosition;
-    smallThumb = new Vector3(.2051785f, .2051785f, .2051785f);
-    largeThumb = new Vector3(.3F, .3F, .3F);
-    SetGazedAt(false);
-  }
+	void Start ()
+	{
+		sceneLoader = GetComponent<LoadOnClick> ();
+		startingPosition = transform.localPosition;
+		smallThumb = new Vector3 (.2051785f, .2051785f, .2051785f);
+		largeThumb = new Vector3 (.3F, .3F, .3F);
+		SetGazedAt (false);
+	}
 
-  void LateUpdate() {
-    Cardboard.SDK.UpdateState();
-    if (Cardboard.SDK.BackButtonPressed) {
-      Application.Quit();
-    }
-  }
+	void LateUpdate ()
+	{
+		Cardboard.SDK.UpdateState ();
+		if (Cardboard.SDK.BackButtonPressed) {
+			Application.Quit ();
+		}
+		RaycastHit hit;
+		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		Debug.Log(isLookedAt);
+		if (!isLookedAt) { delay = Time.time + 2.0f; }
+		if (isLookedAt && Time.time > delay) {
+			sceneLoader.LoadScene (sceneNum);
+		}
+	}
 
-  public void SetGazedAt(bool gazedAt) {
-      Renderer rend = GetComponent<Renderer>();
-      Color color = rend.material.color;
-      if (gazedAt) {
-          color = Color.green;
-          color.a = .4f;
-          rend.transform.localScale = largeThumb;
-      } else {
-          color = Color.red;
-          color.a = 1f;
-          rend.transform.localScale = smallThumb;
-      }
-      rend.material.color = color;
-  }
+	public void SetGazedAt (bool gazedAt)
+	{
+		Renderer rend = GetComponent<Renderer> ();
+		Color color = rend.material.color;
+		if (gazedAt) {
+			color = Color.green;
+			color.a = .4f;
+			rend.transform.localScale = largeThumb;
+		} else {
+			delay = Time.time + 2.0f;
+			color = Color.red;
+			color.a = 1f;
+			rend.transform.localScale = smallThumb;
+		}
+		rend.material.color = color;
+	}
 
-  public void Reset() {
-    transform.localPosition = startingPosition;
-  }
+	public void Reset ()
+	{
+		transform.localPosition = startingPosition;
+	}
 
-  public void ToggleVRMode() {
-    Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
-  }
+	public void ToggleVRMode ()
+	{
+		Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
+	}
 
-  public void ToggleDistortionCorrection() {
-    switch(Cardboard.SDK.DistortionCorrection) {
-    case Cardboard.DistortionCorrectionMethod.Unity:
-      Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Native;
-      break;
-    case Cardboard.DistortionCorrectionMethod.Native:
-      Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.None;
-      break;
-    case Cardboard.DistortionCorrectionMethod.None:
-    default:
-      Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Unity;
-      break;
-    }
-  }
+	public void ToggleDistortionCorrection ()
+	{
+		switch (Cardboard.SDK.DistortionCorrection) {
+		case Cardboard.DistortionCorrectionMethod.Unity:
+			Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Native;
+			break;
+		case Cardboard.DistortionCorrectionMethod.Native:
+			Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.None;
+			break;
+		case Cardboard.DistortionCorrectionMethod.None:
+		default:
+			Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Unity;
+			break;
+		}
+	}
 
-  public void ToggleDirectRender() {
-    Cardboard.Controller.directRender = !Cardboard.Controller.directRender;
-  }
+	public void ToggleDirectRender ()
+	{
+		Cardboard.Controller.directRender = !Cardboard.Controller.directRender;
+	}
 
-  public void TeleportRandomly() {
-    Vector3 direction = Random.onUnitSphere;
-    direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-    float distance = 2 * Random.value + 1.5f;
-    transform.localPosition = direction * distance;
-  }
+	public void TeleportRandomly ()
+	{
+		Vector3 direction = Random.onUnitSphere;
+		direction.y = Mathf.Clamp (direction.y, 0.5f, 1f);
+		float distance = 2 * Random.value + 1.5f;
+		transform.localPosition = direction * distance;
+	}
 
-  #region ICardboardGazeResponder implementation
+	#region ICardboardGazeResponder implementation
 
-  /// Called when the user is looking on a GameObject with this script,
-  /// as long as it is set to an appropriate layer (see CardboardGaze).
-  public void OnGazeEnter() {
-    SetGazedAt(true);
-  }
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see CardboardGaze).
+	public void OnGazeEnter ()
+	{
+		SetGazedAt (true);
+	}
 
-  /// Called when the user stops looking on the GameObject, after OnGazeEnter
-  /// was already called.
-  public void OnGazeExit() {
-    SetGazedAt(false);
-  }
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit ()
+	{
+		SetGazedAt (false);
+	}
 
-  // Called when the Cardboard trigger is used, between OnGazeEnter
-  /// and OnGazeExit.
-  public void OnGazeTrigger() {
-    TeleportRandomly();
-  }
+	// Called when the Cardboard trigger is used, between OnGazeEnter
+	/// and OnGazeExit.
+	public void OnGazeTrigger ()
+	{
+		TeleportRandomly ();
+	}
 
-  #endregion
+	#endregion
 }
