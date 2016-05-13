08fea1b5f7dc016ed9dc68afa6046bbf778f8d2b
Danny <danny@skully.com>
2016-05-13 12:21:06 -0700

    stuff

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Settings : MonoBehaviour {
 
-	// Use this for initialization
 	public static bool VRMODE = false;
 	
 	public bool getVRMODE() {
@@ -12,16 +11,6 @@ public class Settings : MonoBehaviour {
 	
 	public void setVRMODE(bool status) {
 		VRMODE = status;
-		Debug.Log("SET TO " + status);
 	}
 	
-	void Start() {
-		// GameObject camera = GameObject.Find("RearviewMain");
-		// GameObject events = GameObject.Find("EventSystem");
-		// Settings settings = events.GetComponent<Settings>();
-		// setVRMODE(true);
-		// Cardboard vrSetting = camera.GetComponent<Cardboard>();
-		// vrSetting.VRModeEnabled = VRMODE;
-		// Debug.Log(vrSetting.VRModeEnabled);
-	}
 }
